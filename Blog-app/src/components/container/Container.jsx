66eacf31ac7f741c502234a7a7896520bf5a41df
H Fahim <fahim4ctg@gmail.com>
2024-12-12 14:31:01 +0600

    components added

--- Blog-app/src/components/container/Container.jsx
+++ Blog-app/src/components/container/Container.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function Container() {
+export default function Container ({children}) {
   return (
-    <div>Container</div>
+    <div className='w-full max-w-7xl mx-auto px-4'>
+     {children}
+    </div>
   )
 }
 
-export default Container
