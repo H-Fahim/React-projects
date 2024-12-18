615696330834b84d6acdbf007eb79ab15132b0b8
H Fahim <fahim4ctg@gmail.com>
2024-12-18 10:47:19 +0600

    update

--- Blog-app/src/App.jsx
+++ Blog-app/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
 import './App.css'
 import { Outlet } from 'react-router-dom'
---
+++ Blog-app/src/components/header/Header.jsx
@@ -0,0 +1,81 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import {Container} from "../container/Container"
+import {Logo} from "../Logo"
+import {Link , useNavigate} from "react-router-dom"
+import LogoutBtn from "./LogoutBtn"
+import { useSelector } from 'react-redux'
+
+
+
+function Header() {
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus
+    },
+    {
+      name: "Singup",
+      slug: "/singup",
+      active: !authStatus
+    },
+    {
+      name: "All Post",
+      slug: "/allpost",
+      active: authStatus
+    },
+    {
+      name: "Add Post",
+      slug: "/addpost",
+      active: authStatus
+    }
+  ]
+
+  return (
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to="/">
+             <Logo/>
+            </Link>
+          </div>
+          <ul className='flex ml-auto'>
+              {
+                navItems.map((item)=> 
+                  item.active ? 
+                   <li key= {item.name}>
+                      <button
+                        onClick={()=> navigate (item.slug)}
+                        className='inline-block px-6 py-2 
+                        duration-200 hover:bg-blue-100 
+                        rounded-full'>
+                          { item.name }
+                      </button>
+                   </li>
+                 : null )
+              }
+              {
+                authStatus && (
+                  <li>
+                    <LogoutBtn/>
+                  </li>
+                )
+              }
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  )
+}
+
+export default Header
